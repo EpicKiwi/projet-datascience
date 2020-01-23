14ba173289d80abba2e4b265ee775970dec92b46
EpicKiwi <me@epickiwi.fr>
2020-01-24 00:18:57 +0100

    Compilation et completion noise

--- libs/vhs-cli.py
+++ libs/vhs-cli.py
@@ -1,8 +1,8 @@
 from app import CleanApp
-from filters.dummy_filter import DummyFilter
+from filters.denoise_filter import DummyFilter
 
 cli = CleanApp()
 
-cli.add_filter("dummy", DummyFilter)
+cli.add_filter("denoise", DenoiseFilter)
 
 cli.run()
